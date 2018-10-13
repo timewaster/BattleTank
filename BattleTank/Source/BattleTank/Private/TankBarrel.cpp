61ed584858c89713a57f766c24ab2d1a871188ef
Jake Greer <j.greer@aberdeen.ac.uk>
2018-10-14 00:18:22 +0100

    Fixed some needless ticks and renamed a variable for logic reasons.

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,10 +2,10 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float DegreesPerSecond) {
+void UTankBarrel::Elevate(float RelativeSpeed) {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed and the frame time
-	UE_LOG(LogTemp, Warning, TEXT("Barrel Called, speed %f"), DegreesPerSecond);
+	UE_LOG(LogTemp, Warning, TEXT("Barrel Called, speed %f"), RelativeSpeed);
 
 }
 
